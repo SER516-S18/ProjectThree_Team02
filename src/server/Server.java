1f85edaddc47e61cd61cdb8a1bd2d07a0605bf11
Yuan Cao <caoyuan0816@gmail.com>
2018-03-28 04:35:05 -0700

    Sketelon code for Server UI.

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,16 +1,44 @@
 package server;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import javax.websocket.DeploymentException;
 import java.util.Scanner;
 
-public class Server {
+public class Server extends Application {
 
     public static final int PORT = 3000;
     public static final String HOST_NAME = "localhost";
     public static final String ROOT_PATH = "";
     public static final String FACE_ENDPOINT = "face";
 
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     * NOTE: This method is called on the JavaFX Application Thread.
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("server.fxml"));
+        primaryStage.setTitle("Server");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
+
+        launch(args);
 
         org.glassfish.tyrus.server.Server server =
                 new org.glassfish.tyrus.server.Server(
@@ -28,4 +56,5 @@ public class Server {
             server.stop();
         }
     }
+
 }
