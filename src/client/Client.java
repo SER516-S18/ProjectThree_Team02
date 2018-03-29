da7c4839f02ba15a57d8fc054127faaf63ac0565
sanaydevi <sanaydevi@gmail.com>
2018-03-28 21:16:24 -0700

    Client Basic UI

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,14 +1,21 @@
 package client;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.glassfish.tyrus.client.ClientManager;
 
 import javax.websocket.Session;
 import java.net.URI;
 import java.util.Scanner;
+import javafx.*;
+
 
 import static util.JsonUtil.formatMessage;
 
-public class Client {
+public class Client extends Application {
 
     public static final String PORT = "3000";
     public static final String SERVER_ENDPOINT = "face";
@@ -32,6 +39,14 @@ public class Client {
             System.out.println( "Sending to server: " + msg );
             session.getBasicRemote().sendText( msg );
         } while (!message.equalsIgnoreCase("quit"));
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Users/sanaydevi/Downloads/ProjectThree_Team02-master/src/client/Client.fxml"));
+        primaryStage.setTitle("Client");
+        primaryStage.setScene(new Scene(root, 770, 600));
+        primaryStage.show();
+    }
 }
---
+++ src/client/ClientController.java
@@ -0,0 +1,36 @@
+package client;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientController implements Initializable {
+    @FXML
+    private TextField systemStatus;
+    @FXML private TextField systemUpTime;
+    @FXML private ChoiceBox<String> headSet = new ChoiceBox<>();
+    @FXML private TextField Status;
+    @FXML private ChoiceBox<String> profile = new ChoiceBox<>();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        systemStatus.setText("Server running");
+        systemUpTime.setText("40");
+        ObservableList<String> availableChoicesForHeadset = FXCollections.observableArrayList("0","1","2");
+        headSet.setItems(availableChoicesForHeadset);
+        headSet.getSelectionModel().selectFirst();
+        ObservableList<String> availableChoicesForProfile = FXCollections.observableArrayList("jw","Sanay","Dough","Mellisa","Yuan");
+        profile.setItems(availableChoicesForProfile);
+        profile.getSelectionModel().selectFirst();
+        Status.setText("OK");
+
+
+    }
+}
