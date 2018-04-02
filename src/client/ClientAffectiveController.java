e2c178b928aea0ca9ed78182c43528dc23d7f632
sanaydevi <sanaydevi@gmail.com>
2018-04-01 23:38:52 -0700

    Add, Delete Button. Switching scenes on Clicking Affective. Closing Application.

--- src/client/ClientAffectiveController.java
+++ src/client/ClientAffectiveController.java
@@ -1,9 +1,12 @@
 package client;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,6 +14,11 @@ public class ClientAffectiveController implements Initializable {
     @FXML private AnchorPane parent;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    @FXML private void changeToExpressiveScene(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("Client.fxml"));
+        parent.getChildren().setAll(pane);
 
     }
 }
