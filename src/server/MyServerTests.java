f69191893f512eff70479052d834b111544db330
anushaprem29 <anushaprem29@gmail.com>
2018-04-02 16:08:10 -0700

    Client test case

--- src/server/MyServerTests.java
+++ src/server/MyServerTests.java
@@ -9,7 +9,6 @@ public class MyServerTests {
     @Test
     public void testAdd() {
         String jsonPayload = ServerController.getInstance().getJsonMessage();
-
         String str = "Junit Server";
         assertEquals("Junit Server",str);
     }
