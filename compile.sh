23484793f1d8c57dbbd47d4565013facc703e613
dcarrol3 <dcarrol3@asu.edu>
2018-04-10 22:06:04 -0700

    fix compile.sh failure in git bash

--- compile.sh
+++ compile.sh
@@ -20,14 +20,14 @@ echo
 echo "\033[0;32mCompiling Client"
 echo "\033[0;31m"
 
-find "$ROOT_PATH/src" -name *.java > sources.txt
+SOURCES=`find "$ROOT_PATH/src" -name *.java`
 
-javac -d . -cp ".:$ROOT_PATH/src:$LIB_PATH/grizzly-framework-2.3.3.jar:$LIB_PATH/grizzly-http-2.3.3.jar:$LIB_PATH/grizzly-http-server-2.3.3.jar:$LIB_PATH/grizzly-rcm-2.3.3.jar:$LIB_PATH/javax.json-1.0.4.jar:$LIB_PATH/javax.websocket-api-1.0.jar:$LIB_PATH/jcommon-1.0.16.jar:$LIB_PATH/jfreechart-1.0.13.jar:$LIB_PATH/tyrus-client-1.1.jar:$LIB_PATH/tyrus-container-grizzly-1.1.jar:$LIB_PATH/tyrus-core-1.1.jar:$LIB_PATH/tyrus-server-1.1.jar:$LIB_PATH/tyrus-spi-1.1.jar:$LIB_PATH/tyrus-websocket-core-1.1.jar" @sources.txt
+javac -d . -cp ".:$ROOT_PATH/src:$LIB_PATH/grizzly-framework-2.3.3.jar:$LIB_PATH/grizzly-http-2.3.3.jar:$LIB_PATH/grizzly-http-server-2.3.3.jar:$LIB_PATH/grizzly-rcm-2.3.3.jar:$LIB_PATH/javax.json-1.0.4.jar:$LIB_PATH/javax.websocket-api-1.0.jar:$LIB_PATH/jcommon-1.0.16.jar:$LIB_PATH/jfreechart-1.0.13.jar:$LIB_PATH/tyrus-client-1.1.jar:$LIB_PATH/tyrus-container-grizzly-1.1.jar:$LIB_PATH/tyrus-core-1.1.jar:$LIB_PATH/tyrus-server-1.1.jar:$LIB_PATH/tyrus-spi-1.1.jar:$LIB_PATH/tyrus-websocket-core-1.1.jar" $SOURCES
 
 jar cfm client.jar ./manifest/client_manifest.txt lib src client util server model
 rm -rf client
-rm -rf model
-rm -rf sources.txt
+rm -rf server
+rm -rf model	
 
 echo
 echo "\033[0;32mCompile SUCCESS!"
